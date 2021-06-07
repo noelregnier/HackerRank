47c4704bf40758917bbd0b0d0659abcaf1427c23
Kate <53442571+noelregnier@users.noreply.github.com>
2021-06-07 11:58:43 +0300

    Create solution.py

---
+++ Practice/Algorithms/Implementation/Migratory Birds/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    s = set(arr)
+    if arr == s: return min(arr)
+    else:
+        li = [(arr.count(i), i) for i in s]
+        res = min([i[1] for i in li if i[0]==max(y[0] for y in li)])
+        return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
